47c631e372823284ef098881fdd3dbf41fba2552
Dimi11 <dimi11@gmail.com>
2022-06-25 20:55:54 +0200

    Added TSDoc comments.

--- src/ui/UiComponent.ts
+++ src/ui/UiComponent.ts
@@ -4,15 +4,33 @@ export abstract class UiComponent{
     private _decoratorUids: Array<string> = [];
 
     public template: DocumentFragment;
-    protected uiComponentEventHandler: UiComponentEventHandler;
+    private uiComponentEventHandler: UiComponentEventHandler;
 
     constructor(){
         this.template = document.createRange().createContextualFragment(this.render());
         this._decoratorInitElementsWithUids();
     }
 
+    /**
+     * This method must be implemented.
+     * Return value represents html markup of component.
+     * 
+     * @example
+     * protected render():string{
+     *      return '<div><button data-uid="btnTest">Test</button></div>';
+     * } 
+     *
+     * @returns string representing html markup of component 
+     */
     protected abstract render():string;
 
+    /**
+     * Call this method do inject your component.
+     * 
+     * @param container - DOM element in which your component will be added.
+     * @param skipCleaningContainer - if false container will be cleared first otherwise it will append.
+     * @returns 
+     */
     public inject(container: HTMLElement, skipCleaningContainer = false):Node{
         if (!skipCleaningContainer) container.innerHTML = "";
         let n = container.appendChild(this.template.firstChild);
@@ -20,17 +38,38 @@ export abstract class UiComponent{
         return n;
     }
 
+    /**
+     * Override this method if you want to do something with your component right after it is injected in DOM.
+     */
     protected start():void{};
 
+    /**
+     * Use this method to get HTMLElement family object of your component based on data-uid attribute provided in render() method.
+     * You can use @UiElement() annotation instead.
+     * 
+     * @example
+     * let button:HTMLButton = this.findUiElement("btnTest");
+     * 
+     * @param uid - value of data-uid attribute based on which element will be instanced.
+     * @returns Instance of HTMLElement or subclass object.
+     */
     protected findUiElement <T extends HTMLElement>(uid:string):T{
         return this.template.querySelector(`[data-uid="${uid}"]`);
     }
 
 
+    /**
+     * Add event handler that your component can use to emmit events.
+     * 
+     * @param uiComponentEventHandler handler you are setting to be trigered.
+     */
     public addUiComponentEventHandler(uiComponentEventHandler: UiComponentEventHandler):void{
         this.uiComponentEventHandler = uiComponentEventHandler;
     }
 
+    /**
+     * Removes component event handler.
+     */
     public removeUiComponentEventHandler():void{
         this.uiComponentEventHandler = null;
     }
--- src/ui/UiComponentEventHandler.ts
+++ src/ui/UiComponentEventHandler.ts
@@ -1,3 +1,6 @@
+/**
+ * This is functional interface that should be used for setting event handler for UiComponent.
+ */
 export interface UiComponentEventHandler{
     (eventName: string, object?: any):void;
 }
